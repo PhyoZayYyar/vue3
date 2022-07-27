76254c7dc87d735e0f018a5ab86f90383ca4f48a
PhyoZayYyar <phyozayyyar2021@gmail.com>
2022-07-27 10:19:22 +0630

    methods and basic click event

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 const app =Vue.createApp({
     // template:"<h1>I am Learning Vue3</h1>",// using template
     data() {
+        // data injection 
         return {
             name:"Phyo Si Thu Or Phyo Zay Yyar",
             sex:false,
             age:28
+        }
+    },
+    methods:{
+        increaseAge(){
+            // alert(age)
+            this.age++
         }
     }
 });
